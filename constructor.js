425a7e3157af1f73c1b45ba135874ebbc02021ae
Jason Stiltner <jasonstiltner@gmail.com>
2016-01-18 15:15:46 -0600

    classwork

---
+++ constructor.js
@@ -0,0 +1,6 @@
+function Boa (){
+};
+Boa.prototype.poop = function (){
+  console.log('💩');
+};
+module.exports = Boa;
---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('This is an index file')
+
+var x = 'test';
+var y = function() {};
+
+module.exports = {a:'b'};
